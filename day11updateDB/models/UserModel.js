42f5ae400165e1351e5540b060bb2971db744547
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-05-26 12:45:13 +0530

    udpate with dB

---
+++ day11updateDB/models/UserModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  gender: {
+    type: String,
+    require: true,
+  },
+  hobby: {
+    type: Array,
+    require: true,
+  },
+});
+
+const UserModel = mongoose.model("userData", userSchema);
+
+module.exports = UserModel;
