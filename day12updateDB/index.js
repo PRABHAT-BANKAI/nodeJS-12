d01ff68458534f84a1bcd6fe0a0f54cd00e14d64
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-05-28 12:47:59 +0530

    multerupdatedelete

--- day12updateDB/index.js
+++ day12updateDB/index.js
@@ -4,11 +4,12 @@ const UserModel = require("./models/UserModel");
 const upload = require("./middlware/multer");
 const port = 8081;
 const app = express();
-const path = require("path")
+const path = require("path");
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads",express.static(path.join(__dirname,"/uploads")))
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.get("/", async (req, res) => {
   try {
     let userData = await UserModel.find({});
@@ -22,10 +23,9 @@ app.get("/", async (req, res) => {
 
 app.post("/addData", upload, async (req, res) => {
   try {
-if(req.file){
-req.body.image = "/uploads"+"/"+req.file.filename
-
-}
+    if (req.file) {
+      req.body.image = "/uploads" + "/" + req.file.filename;
+    }
     await UserModel.create(req.body);
     console.log("user Data added successfully");
     res.redirect("/");
@@ -35,10 +35,14 @@ req.body.image = "/uploads"+"/"+req.file.filename
   }
 });
 
-app.get("/deleteData/:id", async (req, res) => {
-  const id = req.params.id; // _id private//
+app.get("/deleteData/", async (req, res) => {
+  // // const id = req.params.id; // _id private//
+  // console.log(req.query);
+  const id = req.query.dataId;
+  const userData = await UserModel.findById(id); // userData get (userData.image(/uploads/filename))
 
   try {
+    fs.unlinkSync(path.join(__dirname, userData.image)); // delete image from uploads folder
     await UserModel.findByIdAndDelete(id);
     console.log("user is deleted successfully");
     res.redirect("/");
@@ -55,12 +59,20 @@ app.get("/editData/:id", async (req, res) => {
   res.render("edit", { userData });
 });
 
-app.post("/updateData", async (req, res) => {
+app.post("/updateData", upload,async (req, res) => {
   // console.log(req.body);
 
   try {
-    await UserModel.findByIdAndUpdate(req.body.id, req.body); //query
-    console.log("userData updated successfully");
+    const userData = await UserModel.findById(req.body.id);
+
+    if (req.file) {
+   
+      fs.unlinkSync(path.join(__dirname, userData.image)); // delete old image from uploads folder
+      req.body.image = "/uploads" + "/" + req.file.filename; // update new image path
+  
+    }
+        await UserModel.findByIdAndUpdate(req.body.id, req.body); //query
+      console.log("userData updated successfully");
 
     res.redirect("/");
   } catch (error) {
